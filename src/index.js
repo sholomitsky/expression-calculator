af95aaf16471cb911438b440eb7ff15cfad33787
sholomitsky <antonsholom@mail.ru>
2020-03-12 18:45:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let left = [],
+        right = [];
+    let arr = expr.split('');
+    for (a = 0; a <= arr.length - 1; a++) {
+        if (arr[a] === '(') {
+            left.push(arr[a]);
+        }
+        if (arr[a] === ')') {
+            right.push(arr[a]);
+        }
+        if ((arr[a] + arr[a + 1] + arr[a + 2]) === '/ 0') {
+            throw new Error('TypeError: Division by zero.')
+        }
+    }
+    if (left.length === right.length) {
+        return new Function('return ' + expr)();
+    } else {
+        throw ("ExpressionError: Brackets must be paired");
+    }
 }
 
 module.exports = {
